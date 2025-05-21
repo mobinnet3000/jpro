7b15dec9dab3481db090301ebe91e6b302ae490d
MobiN <mobinnet3000@gmail.com>
2025-05-21 04:07:47 -0700

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'riferal platform',
+      title: 'jpro',
       locale: Locale('en'),
       theme: ThemeData(
         primarySwatch: Colors.blue,
